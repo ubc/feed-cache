70deed72b7bffdc735d785a3ab4b5e378f3e57ca
Michael Ha <mha.hk.ca@gmail.com>
2013-02-28 09:48:22 -0800

    initial file

---
+++ feed-cache.php
@@ -0,0 +1,10 @@
+<?php
+/*
+Plugin Name: Feed Cache
+Plugin URI: https://github.com/ubc/feed-cache
+Description: This plugin allows administrator of a WordPress site to set the RSS feed cache duration.
+Version: 0.1
+Author: CTLT
+Author URI: http://ctlt.ubc.ca
+License: GPLv2
+*/
